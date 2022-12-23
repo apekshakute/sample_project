da87ab5ca0bb57efd8b90f8663bf85ae46d0565a
apekshakute <64271681+apekshakute@users.noreply.github.com>
2022-12-23 11:12:21 +0530

    create number_palindrome.py

---
+++ number_palindrome.py
@@ -0,0 +1,32 @@
+ip=int(input("input  :"))
+input_1=ip
+found=False
+while(found==False):
+    str1=str(input_1)[::-1]
+    if str1 == str(input_1):
+        found=True
+        print(f"palindrome: {str1}")
+        next_palindrome=str1
+        break
+    else:
+        input_1 += 1
+
+input_1=ip
+found=False
+while(found==False):
+    str1=str(input_1)[::-1]
+    if str1 == str(input_1):
+        found=True
+        print(f"palindrome: {str1}")
+        prev_palindrome=str1
+        break
+    else:
+        input_1 = input_1- 1
+near_palindrome=""  
+print(int(next_palindrome) - int(ip))
+print(int(ip) - int(prev_palindrome))
+if (int(next_palindrome) - int(ip)) > (int(ip) - int(prev_palindrome)):
+    near_palindrome=prev_palindrome
+else:
+    near_palindrome=next_palindrome
+print(f"near palindrome:{near_palindrome}")
